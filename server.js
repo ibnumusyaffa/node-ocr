c40b530e4d9e549730d10f2416323b783f9e39d1
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2021-07-04 19:13:50 +0700

    add environment test

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const path = require("path");
+if (process.env.NODE_ENV == "test") {
+  require("dotenv").config({
+    path: path.resolve(process.cwd(), ".env.test"),
+  });
+} else {
+  require("dotenv").config();
+}
+
 const app = express();
 const port = process.env.NODE_PORT || 3000;
 
@@ -10,7 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server listering at port ${port}`);
+  console.log(`ENV : ${process.env.NODE_ENV}`);
 });
 
-require('./app/routes')(app)
+require("./app/routes")(app);
 module.exports = app;
