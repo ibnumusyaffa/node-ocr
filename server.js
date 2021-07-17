0107cc32fa6046e8797691203250daef84cd55ac
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2021-07-17 08:57:54 +0700

    add commo middleware

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const compression = require('compression');
+const helmet = require("helmet");
+
 if (process.env.NODE_ENV == 'test') {
   require('dotenv').config({
     path: path.resolve(process.cwd(), '.env.test'),
@@ -13,8 +16,14 @@ const app = express();
 const port = process.env.NODE_PORT || 3000;
 
 app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(compression())
+app.use(helmet());
 
 app.listen(port, () => {
   console.log(`Server listering at port ${port}`);
