a90b318a2993c53cd79ecf724642ef4f918f1edb
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2021-08-06 20:57:10 +0700

    add docker compose

--- tests/halo.js
+++ tests/halo.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 describe('Array', function () {
   describe('#indexOf()', function () {
+    console.log(process.env.NODE_ENV)
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
