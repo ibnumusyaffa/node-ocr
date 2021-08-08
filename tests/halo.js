725d19f596739cf65dcb152a67780dc437afec10
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2021-08-08 08:31:33 +0700

    remove mocha chai

--- tests/halo.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    // console.log(process.env.NODE_ENV)
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
