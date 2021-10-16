df74f3ac96a7b6fdecc179c27f8ee95f6053fbed
ibnu musyaffa <ibnu.musyaffa@gmail.com>
2021-10-16 17:33:25 +0700

    update

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,31 +1,50 @@
-const { readPdfText } = require('pdf-text-reader');
-var v = require('voca');
-exports.index = async (req, res) => {
-  try {
-    let pages = await readPdfText(req.file.path);
-    pages = pages
-      .map((page) => {
-        let index = page.lines.findIndex((item) =>
-          v.includes(item.toLocaleLowerCase(), 'pr number')
-        );
-        return page.lines[index + 1];
-      })
-      .map((item) => item.split('/')[0])
-      .map((item) => item.split('     '))
-      .map((item) => {
-        return item
-          .map((item) => item.trim())
-          .filter((item) => v.isDigit(item));
-      })
-      .map((item) => item[0]);
+const { PDFNet } = require('@pdftron/pdfnet-node');
 
-    return res.send({
-      message: [...new Set(pages)],
+exports.index = async (req, res, next) => {
+  try {
+    await PDFNet.initialize(
+      'demo:1634379058697:78a1fe8f0300000000861dbdb2cd9281480c0902d97794810747724cbd'
+    );
+    await PDFNet.runWithCleanup(async () => {
+      const pdfdoc = await PDFNet.PDFDoc.createFromFilePath(req.file.path);
+      await pdfdoc.initSecurityHandler();
+      const page = await pdfdoc.getPage(1);
+      const txt = await PDFNet.TextExtractor.create();
+
+      let rect = null;
+      let textResult = '';
+      if (req.body.type == 'pr') {
+        rect = new PDFNet.Rect(250, 449, 397, 450);
+        txt.begin(page, rect);
+        textResult = await txt.getAsText();
+        if (textResult) {
+          textResult = textResult.split('/')[0];
+        }
+      }
+      if (req.body.type == 'sa') {
+        rect = new PDFNet.Rect(400, 490, 912, 500);
+        txt.begin(page, rect);
+        textResult = await txt.getAsText();
+        if (textResult) {
+          textResult = textResult.split('/')[0];
+        }
+      }
+      if (req.body.type == 'po') {
+        rect = new PDFNet.Rect(200, 441, 350, 442);
+        txt.begin(page, rect);
+        textResult = await txt.getAsText();
+        if (textResult) {
+          textResult = textResult.split('/')[0];
+        }
+      }
+
+      res.send({
+        result: textResult,
+      });
     });
   } catch (error) {
-    console.log('error');
-    return res.send({
-      message: error.message,
+    res.send({
+      result: 'error',
     });
   }
 };
